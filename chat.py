ae82fe280ab286023b198ef421a4430ab9355cf5
David Shapiro <daveshap37@gmail.com>
2023-01-16 05:22:01 -0500

    working

--- chat.py
+++ chat.py
@@ -2,10 +2,10 @@ import os
 import openai
 import json
 import numpy as np
-import textwrap
+from numpy.linalg import norm
 import re
 from time import time,sleep
-import wikipedia
+from uuid import uuid4
 
 
 def open_file(filepath):
@@ -13,45 +13,87 @@ def open_file(filepath):
         return infile.read()
 
 
-openai.api_key = open_file('openaiapikey.txt')
+def save_file(filepath, content):
+    with open(filepath, 'w', encoding='utf-8') as outfile:
+        outfile.write(content)
 
 
-def gpt3_embedding(content, engine='text-similarity-ada-001'):
+def load_json(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return json.load(infile)
+
+
+def save_json(filepath, payload):
+    with open(filepath, 'w', encoding='utf-8') as outfile:
+        json.dump(payload, outfile, ensure_ascii=False, sort_keys=True, indent=2)
+
+
+def gpt3_embedding(content, engine='text-embedding-ada-002'):
     content = content.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Embedding.create(input=content,engine=engine)
     vector = response['data'][0]['embedding']  # this is a normal list
     return vector
 
 
-def similarity(v1, v2):  # return dot product of two vectors
-    return np.dot(v1, v2)
+def similarity(v1, v2):
+    # based upon https://stackoverflow.com/questions/18424228/cosine-similarity-between-2-number-lists
+    return np.dot(v1, v2)/(norm(v1)*norm(v2))  # return cosine similarity
 
 
-def save_debug(label, content):
-    filename = '%s_%s.txt' % (time(), label)
-    with open('debug/%s' % filename, 'w') as outfile:
-        outfile.write(content)
-
-
-def search_index(recent, all_lines, count=10):
-    if len(all_lines) <= count:
-        return list()
+def fetch_memories(vector, logs, count):
     scores = list()
-    for i in all_lines:
-        if recent['vector'] == i['vector']:
+    for i in logs:
+        if vector == i['vector']:
+            # skip this one because it is the same message
             continue
-        score = similarity(recent['vector'], i['vector'])
-        #print(score)
-        scores.append({'line': i['line'], 'score': score})
+        score = similarity(i['vector'], vector)
+        i['score'] = score
+        scores.append(i)
     ordered = sorted(scores, key=lambda d: d['score'], reverse=True)
+    # TODO - pick more memories temporally nearby the top most relevant memories
     try:
         ordered = ordered[0:count]
-        return [i['line'] for i in ordered]
+        return ordered
     except:
-        return [i['line'] for i in ordered]
+        return ordered
 
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['USER:', 'TIM:']):
+def load_convo():
+    files = os.listdir('chat_logs')
+    files = [i for i in files if '.json' in i]  # filter out any non-JSON files
+    result = list()
+    for file in files:
+        data = load_json('chat_logs/%s' % file)
+        result.append(data)
+    ordered = sorted(result, key=lambda d: d['time'], reverse=False)  # sort them all chronologically
+    return result
+
+
+def summarize_memories(memories):  # summarize a block of memories into one payload
+    memories = sorted(memories, key=lambda d: d['time'], reverse=False)  # sort them chronologically
+    block = ''
+    for mem in memories:
+        block += '%s: %s\n\n' % (mem['speaker'], mem['message'])
+    block = block.strip()
+    prompt = open_file('prompt_notes.txt').replace('<<INPUT>>', block)
+    # TODO - do this in the background over time to handle huge amounts of memories
+    notes = gpt3_completion(prompt)
+    return notes
+
+
+def get_last_messages(conversation, limit):
+    try:
+        short = conversation[-limit:]
+    except:
+        short = conversation
+    output = ''
+    for i in short:
+        output += '%s: %s\n\n' % (i['speaker'], i['message'])
+    output = output.strip()
+    return output
+
+
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.0, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['USER:', 'RAVEN:']):
     max_retry = 5
     retry = 0
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
@@ -67,10 +109,12 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
                 presence_penalty=pres_pen,
                 stop=stop)
             text = response['choices'][0]['text'].strip()
-            text = re.sub('\s+', ' ', text)
+            text = re.sub('[\r\n]+', '\n', text)
+            text = re.sub('[\t ]+', ' ', text)
             filename = '%s_gpt3.txt' % time()
-            with open('gpt3_logs/%s' % filename, 'w') as outfile:
-                outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
+            if not os.path.exists('gpt3_logs'):
+                os.makedirs('gpt3_logs')
+            save_file('gpt3_logs/%s' % filename, prompt + '\n\n==========\n\n' + text)
             return text
         except Exception as oops:
             retry += 1
@@ -80,66 +124,28 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
             sleep(1)
 
 
-def fetch_wiki(title):
-    try:
-        page = wikipedia.page(title)
-        return page
-    except:
-        search = wikipedia.search(title)
-        page = wikipedia.page(search[0])
-        return page
-
-
-def answer_question(article, question):
-    chunks = textwrap.wrap(article, 10000)
-    answers = list()
-    for chunk in chunks:
-        prompt = open_file('prompt_answer.txt').replace('<<PASSAGE>>', chunk).replace('<<QUESTION>>', question)
-        answer = gpt3_completion(prompt)
-        answers.append(answer)
-    if len(answers) == 1:
-        return answers[0]
-    answer = ' '.join(answers)
-    prompt = open_file('prompt_merge.txt').replace('<<QUESTION>>', question).replace('<<ANSWERS>>', answer)
-    answer = gpt3_completion(prompt)
-    return answer
-
-
 if __name__ == '__main__':
-    conversation = list()
+    openai.api_key = open_file('openaiapikey.txt')
     while True:
-        # get user input and vector
-        user_says = input("USER: ")
-        line_in = 'USER: %s' % user_says
-        vector = gpt3_embedding(line_in)
-        info = {'line': line_in, 'vector': vector}
-        conversation.append(info)
-        # search conversation for previous relevant lines of dialog
-        old_lines = search_index(info, conversation, 10)
-        recent_conversation = [i['line'] for i in conversation]
-        if len(recent_conversation) > 30:
-            recent_conversation = recent_conversation[-30:0]
-        convo_block = '\n'.join(old_lines) + '\n' + '\n'.join(recent_conversation)
-        convo_block = convo_block.strip()
-        save_debug('convo', convo_block)
-        # generate a search query to find external article in the wide world
-        prompt = open_file('prompt_wikipedia.txt').replace('<<BLOCK>>', convo_block)
-        title = gpt3_completion(prompt)
-        save_debug('wiki title', title)
-        wiki = fetch_wiki(title).content.encode(encoding='ASCII',errors='ignore').decode()
-        save_debug('wiki article', wiki)
-        # generate a specific follow-up question to use to query the external information
-        prompt = open_file('prompt_followup.txt').replace('<<BLOCK>>', convo_block).replace('<<TOPIC>>', title)
-        question = gpt3_completion(prompt)
-        save_debug('question', question)
-        answer = answer_question(wiki, question)
-        save_debug('answer', answer)
-        # populate the chat prompt
-        prompt = open_file('prompt_chat.txt').replace('<<BLOCK>>', convo_block).replace('<<HINT>>', answer)
-        response = gpt3_completion(prompt)
-        # save the output
-        vector = gpt3_embedding(response)
-        line_out = 'TIM: %s' % response
-        info = {'line': line_out, 'vector': vector}
-        conversation.append(info)
-        print(line_out)
+        #### get user input, save it, vectorize it, etc
+        a = input('\n\nUSER: ')
+        vector = gpt3_embedding(a)
+        info = {'speaker': 'USER', 'time': time(), 'vector': vector, 'message': a, 'uuid': str(uuid4())}
+        filename = 'log_%s_USER.json' % time()
+        save_json('chat_logs/%s' % filename, info)
+        #### load conversation
+        conversation = load_convo()
+        #### compose corpus (fetch memories, etc)
+        memories = fetch_memories(vector, conversation, 10)  # pull episodic memories
+        # TODO - fetch declarative memories (facts, wikis, KB, company data, internet, etc)
+        notes = summarize_memories(memories)
+        recent = get_last_messages(conversation, 4)
+        prompt = open_file('prompt_response.txt').replace('<<NOTES>>', notes).replace('<<CONVERSATION>>', recent)
+        #### generate response, vectorize, save, etc
+        output = gpt3_completion(prompt)
+        vector = gpt3_embedding(output)
+        info = {'speaker': 'RAVEN', 'time': time(), 'vector': vector, 'message': output, 'uuid': str(uuid4())}
+        filename = 'log_%s_RAVEN.json' % time()
+        save_json('chat_logs/%s' % filename, info)
+        #### print output
+        print('\n\nRAVEN: %s' % output) 
